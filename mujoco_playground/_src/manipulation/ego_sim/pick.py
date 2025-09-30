0bec9220183a5f5cf3edb5934e782cbce1fa2ae4
Omar <olr7742@nyu.edu>
2025-09-29 20:42:30 -0700

    update quat

--- mujoco_playground/_src/manipulation/ego_sim/pick.py
+++ mujoco_playground/_src/manipulation/ego_sim/pick.py
@@ -160,7 +160,7 @@ class RUMPickCube(rum.RUMGripper):
         new_position = current_pos + delta_action[:3]
         current_rot = state.data.site_xmat[self._gripper_site].copy().reshape(3, 3)
         new_rotation = current_rot @ euler_to_mat(delta_action[3:6])
-        new_quat = R.from_matrix(new_rotation).as_quat()
+        new_quat = mat_to_quat(new_rotation)
         new_quat = jp.array([new_quat[3], new_quat[0], new_quat[1], new_quat[2]])
 
         # Update mocap data in one operation
